cb6d06de86fe13d2b5f734b52aa546a633005aa7
neptali <njpm28@gmail.com>
2024-06-06 14:59:14 -0400

    delete procfile

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-origins = [
-    "https://resulting-marilyn-nepta-system-f4da9789.koyeb.app",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/")
